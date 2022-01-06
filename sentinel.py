5d1e7ae74cc5f390386ddcdf656f46c9525ac4c0
alias-bitsignal <blogbar+1@gmail.com>
2022-01-06 20:01:49 +0100

    cron sentinel added

---
+++ sentinel.py
@@ -0,0 +1,20 @@
+"""
+sentinel for explorer
+
+Cron sentinel / @bitsignal
+To be run every minute.
+Updated for Python3.7 by default
+"""
+import subprocess
+
+# Edit this if you are not using the standard invocation
+PYTHON_EXECUTABLE='python3.7'
+
+list_ = subprocess.getoutput("screen -ls")
+try:
+    if ".explorer\t" not in list_:
+        print("Restarting stopped explorer...")
+        data = subprocess.getoutput('screen -d -S explorer -m bash -c "{} explorebis.py" -X quit'.format(PYTHON_EXECUTABLE))
+        print("started")
+except:
+    pass
